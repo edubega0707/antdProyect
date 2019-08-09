421368a5e6dcd3cca2e32da6d88f166cee15b3a0
Eduardo Benitez <eduisic94@gmail.com>
2019-08-09 16:17:18 -0500

    working with antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ConfigProvider } from 'antd';
 import './App.css';
+import { DatePicker, Button } from 'antd';
+import español from 'antd/es/locale/es_ES';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={español}>
+      <div className="App">
+        <DatePicker />
+        <Button type="primary" icon="search" >Click</Button>
+      </div>
+    </ConfigProvider>
+  
   );
 }
 
